e2881e07c322e0cccb90717d5a822353f85f5464
Vergil Phan <123117700+vergilht@users.noreply.github.com>
2023-11-30 04:28:56 -0700

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+import { GetFights } from "./db.js";
+import { GetFighters } from "./db.js";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5100;
+
+// Use middleware to parse JSON
+app.use(bodyParser.json());
+
+// Serve static files from the client directory
+app.use(express.static("../client/build"));
+
+app.get("/", (req, res) => {
+    res.send("Server connected with React.js");
+});
+
+// API endpoints
+app.get("/api/fights", async (req, res) => {
+    try {
+        const fights = await GetFights();
+        res.status(200).json(fights);
+    } catch (error) {
+        console.error("Error during getting fight events:", error);
+        res.status(500).json({ error: "Error getting fights events" });
+    }
+});
+
+app.get("/api/fighters", async (req, res) => {
+    try {
+        const fighters = await GetFighters();
+        res.status(200).json(fighters);
+    }
+    catch (error) {
+        console.error("Error during getting fighters profile:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+app.get("/api/predictions", (req, res) => {
+    // Logic to get predictions data
+    const fightPredictions = [
+        {
+            fighter1: "Fighter A",
+            fighter2: "Fighter B",
+            prediction: "Fighter A wins",
+        },
+        // Add more predictions as needed
+    ];
+    res.json(fightPredictions);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

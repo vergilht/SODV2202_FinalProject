67ed3877a08e686c1672918fdd04350bb1fab0a3
Vergil Phan <vergil.phan@gmail.com>
2023-11-30 05:14:06 -0700

    Edit some pages to make the code works

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Router, Routes, Route } from 'react-router-dom';
 import Fighters from './pages/Fighters';
 
-const App = () => {
+function App () {
   return (
+      <>
       <Routes>
-        <Route exact path="/fighters" component={Fighters} />
+        <Route path="/" element={<Fighters />} />
       </Routes>
+      </>
   );
-};
+}
 export default App;
--- frontend/src/components/pages/Fighters.js
+++ frontend/src/components/pages/Fighters.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import './Fighters.css';
+import { Link } from 'react-router-dom';
 
 function Fighters() {
     const [fighters, setFighters] = useState([]);

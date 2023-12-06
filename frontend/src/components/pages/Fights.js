78a860c4c9a71a3e1b31ce24936b724e5dec76df
jiwonjeon <j.jeon339@mybvc.ca>
2023-12-06 16:39:47 -0700

    Fixed useEffect for fetchFights

--- frontend/src/components/pages/Fights.js
+++ frontend/src/components/pages/Fights.js
@@ -3,21 +3,13 @@ import React, { useState, useEffect } from "react";
 function Fights() {
   const [upcomingFights, setUpcomingFights] = useState([]);
   const [offset, setOffset] = useState(0);
-  /*   useEffect(() => {
-    // Fetch upcoming fights data from the server
-    fetch("/api/fights")
-      .then((response) => response.json())
-      .then((data) => setUpcomingFights(data))
-      .catch((error) =>
-        console.error("Error fetching upcoming fights:", error)
-      );
-  }, []); */
 
   const fetchFights = async () => {
     try {
-      const response = await fetch(`/api/fights?offset=${offset}&limit=5`);
+      const response = await fetch(`/api/fights?offset=${offset}`);
       const data = await response.json();
       setUpcomingFights((prevFights) => [...prevFights, ...data]);
+      console.log(upcomingFights);
       setOffset((preOffset) => preOffset + 5);
     } catch (error) {
       console.error("Error fetching fights:", error);
@@ -26,7 +18,7 @@ function Fights() {
 
   useEffect(() => {
     fetchFights();
-  });
+  }, []);
 
   return (
     <div>

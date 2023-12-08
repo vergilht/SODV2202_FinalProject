a9d5c90074a53251ecd3ee1777e658caadd88ef4
jiwonjeon <j.jeon339@mybvc.ca>
2023-12-08 15:22:08 -0700

    Moved the prediction function to db.js

--- backend/db.js
+++ backend/db.js
@@ -38,26 +38,9 @@ const executeQuery = async (query, params) => {
   }
 };
 
-// GET fights
-/*export const GetFights = async function () {
-  const query = `
-      SELECT * FROM fight
-      SELECT * FROM fight
-      `;
-
-  try {
-    const result = await executeQuery(query, []);
-
-    return result;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-}; */
-
 export const GetFightsWithIndex = async function (offset) {
   try {
-    const limit = 5;
+    const limit = 3;
     const query = `
           SELECT f.*, ft1.*, ft2.*
           FROM fight f
@@ -105,4 +88,83 @@ export const GetFighterById = async (id) => {
     console.error(err);
     throw err;
   }
+};
+
+export const calcFighterPrediction = (fighter1, fighter2) => {
+  let f1 = 0;
+  let f2 = 0;
+
+  if (fighter1.height > fighter2.height) {
+    f1 = f1 + 1;
+  } else if (fighter1.height < fighter2.height) {
+    f2 = f2 + 1;
+  } else {
+    f1 = f1 + 1;
+    f2 = f2 + 1;
+  }
+
+  if (fighter1.weight > fighter2.weight) {
+    f1 = f1 + 1;
+  } else if (fighter1.weight < fighter2.weight) {
+    f2 = f2 + 1;
+  } else {
+    f1 = f1 + 1;
+    f2 = f2 + 1;
+  }
+
+  if (fighter1.wins > fighter2.wins) {
+    f1 = f1 + 1;
+  } else if (fighter1.wins < fighter2.wins) {
+    f2 = f2 + 1;
+  } else {
+    f1 = f1 + 1;
+    f2 = f2 + 1;
+  }
+
+  if (fighter1.reach > fighter2.reach) {
+    f1 = f1 + 1;
+  } else if (fighter1.reach < fighter2.reach) {
+    f2 = f2 + 1;
+  } else {
+    f1 = f1 + 1;
+    f2 = f2 + 1;
+  }
+
+  if (fighter1.stance == "Switch") {
+    var stance1 = 3;
+  } else if (fighter1.stance == "Southpaw") {
+    stance1 = 2;
+  } else {
+    stance1 = 1;
+  }
+
+  if (fighter2.stance == "Switch") {
+    var stance2 = 3;
+  } else if (fighter2.stance == "Southpaw") {
+    stance2 = 2;
+  } else {
+    stance2 = 1;
+  }
+
+  if (stance1 > stance2) {
+    f1 = f1 + 1;
+  } else if (stance1 < stance2) {
+    f2 = f2 + 1;
+  } else {
+    f1 = f1 + 1;
+    f2 = f2 + 1;
+  }
+
+  if (fighter1.ranking < fighter2.ranking) {
+    f1 = f1 + 2;
+  } else if (fighter1.raking > fighter2.raking) {
+    f2 = f2 + 2;
+  } else {
+    f1 = f1 + 2;
+    f2 = f2 + 2;
+  }
+  const probFighter1 = (f1 * 100) / (f1 + f2);
+  const probFighter2 = (f2 * 100) / (f1 + f2);
+
+  return { fighter1: probFighter1, fighter2: probFighter2 };
 };
